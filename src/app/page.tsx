374154005d3978277f579ce960a6f55aff9cdf2e
Gabriel Raducu <Gabriel.Raducu@contracted.pmi.com>
2023-09-18 15:19:46 +0300

    chore: setup button

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="container w-3/5 py-5">
+      <button data-provider="google" className="p-2 rounded-md mx-auto bg-blue-500 text-white click-btn">Click me</button>
+    </div>
+  )
+}
