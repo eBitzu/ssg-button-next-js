c3ab04fb56da096cc010233aab00acead64e8f1a
Gabriel Raducu <Gabriel.Raducu@contracted.pmi.com>
2023-09-19 11:16:20 +0300

    chore: try direct cookie

--- src/components/button.server.tsx
+++ src/components/button.server.tsx
@@ -1,13 +1,8 @@
 import { cookies } from "next/headers";
 import { SignInFunction } from "../actions/sign-in/sign-in.action";
-import { rootURL } from "../env/route.env";
 
 export async function ButtonServer() {
-  const color = await fetch(`${rootURL}/api`, { credentials: "include" })
-    .then(() => {
-      return cookies().get("x-color")?.value ?? "blue";
-    })
-    .catch(() => 'red');
+  const color = cookies().get('x-color')?.value ?? 'blue';
   return (
     <form action={SignInFunction}>
       <input type="hidden" value={color} name="color" />
