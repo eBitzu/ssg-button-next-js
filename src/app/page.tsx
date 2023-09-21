da4b4296dc39e64725080612fe4eeb7ef696d08e
Gabriel Raducu <Gabriel.Raducu@contracted.pmi.com>
2023-09-21 11:00:15 +0300

    chore: fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return <section className="w-screen" style={{height: 'calc(100vh - 82px)'}}>
-      <iframe className="h-full w-full" src="http://localhost:3001" />
+      <iframe className="h-full w-full" src="https://gr-cv.vercel.app" />
   </section>
 }
