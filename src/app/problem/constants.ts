6cb494e82657f2b0635ad2740d14d736788a7f0f
Gabriel Raducu <graducu@amerilife.com>
2023-11-01 09:26:00 +0200

    fix: improve code in pre

--- src/app/problem/constants.ts
+++ src/app/problem/constants.ts
@@ -13,18 +13,17 @@ export default async function PageDefault() {
   );
 }
 // button.client.tsx
-export const ClientButton: FC<ClientButtonProps> = ({ data, extended }) => {
+export const ClientButton: FC<ClientButtonProps> = ({ data, title }) => {
   const [state, setState] = useState(true);
   const [sData, setData] = useState(data);
   const handleClick = () => {
     setState((old) => !old);
-    if(extended) {
-      fetchData().then(setData);
-    }
+    fetchData().then(setData);
   }
   return (
     <button
       onClick={handleClick}
+      title={title}
       className={\`bg-\${state ? "blue" : "red"}-500\`}
     >
       with onClick - {sData}
